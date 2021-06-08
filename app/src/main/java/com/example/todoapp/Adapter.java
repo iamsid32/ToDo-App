07ca0520c0950a2b8ed39a5d4e734d78fa2a8785
iamsid32 <siddharth@edugorilla.org>
2021-06-08 16:39:06 +0530

    first commit

---
+++ app/src/main/java/com/example/todoapp/Adapter.java
@@ -0,0 +1,122 @@
+package com.example.todoapp;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.AdapterViewHolder> {
+
+    private List<String> tasks_list;
+    private Context context;
+
+    public Adapter(List<String> tasks_list, Context context) {
+        this.tasks_list = tasks_list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new AdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.AdapterViewHolder holder, int position) {
+        String task = tasks_list.get(position);
+        holder.tv_task.setText(task);
+        holder.tv_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog show_task_dialog = new Dialog(context);
+                show_task_dialog.setContentView(R.layout.task_description);
+                show_task_dialog.setCanceledOnTouchOutside(false);
+                show_task_dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                WindowManager.LayoutParams layout_params = new WindowManager.LayoutParams();
+                layout_params.copyFrom(show_task_dialog.getWindow().getAttributes());
+                layout_params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                layout_params.height = WindowManager.LayoutParams.MATCH_PARENT;
+                show_task_dialog.getWindow().setAttributes(layout_params);
+                TextView text_description = show_task_dialog.findViewById(R.id.text_description);
+                text_description.setText(task);
+                show_task_dialog.show();
+            }
+        });
+        holder.iv_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog edit_dialog = new Dialog(context);
+                edit_dialog.setContentView(R.layout.edit_dialog);
+                edit_dialog.setCanceledOnTouchOutside(false);
+                edit_dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                WindowManager.LayoutParams layout_params = new WindowManager.LayoutParams();
+                layout_params.copyFrom(edit_dialog.getWindow().getAttributes());
+                layout_params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                layout_params.height = WindowManager.LayoutParams.MATCH_PARENT;
+                edit_dialog.getWindow().setAttributes(layout_params);
+                EditText et_edit_here = edit_dialog.findViewById(R.id.et_edit_here);
+                et_edit_here.setText(holder.tv_task.getText());
+                TextView tv_edit = edit_dialog.findViewById(R.id.tv_edit);
+                edit_dialog.show();
+                tv_edit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (et_edit_here.getText().toString().isEmpty()){
+                            Toast.makeText(context, "Can't create empty events!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            holder.tv_task.setText(et_edit_here.getText());
+                            tasks_list.set(position, String.valueOf(et_edit_here.getText()));
+                            notifyItemChanged(position);
+                            SharedPreferences sharedPreferences = context.getSharedPreferences("com.example.todoapp", Context.MODE_PRIVATE);
+                            HashSet<String> set = new HashSet(tasks_list);
+                            sharedPreferences.edit().putStringSet("addNote", set).apply();
+                            edit_dialog.dismiss();
+                        }
+                    }
+                });
+            }
+        });
+        holder.iv_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tasks_list.remove(position);
+                notifyItemRemoved(position);
+                notifyDataSetChanged();
+                SharedPreferences sharedPreferences = context.getSharedPreferences("com.example.todoapp", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet(tasks_list);
+                sharedPreferences.edit().putStringSet("addNote", set).apply();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks_list.size();
+    }
+
+    public class AdapterViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_task;
+        ImageView iv_edit, iv_delete;
+        public AdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tv_task = itemView.findViewById(R.id.tv_task);
+            iv_edit = itemView.findViewById(R.id.iv_edit);
+            iv_delete = itemView.findViewById(R.id.iv_delete);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    public static List<String> list;
+    Adapter adapter;
+    private CardView card_view_add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        Window w = getWindow();
+//        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getSupportActionBar().hide();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        card_view_add_button = findViewById(R.id.card_view_add_button);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.todoapp", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("addNote", null);
+
+        if (set != null){
+            list = new ArrayList<>(set);
+        } else {
+            list = new ArrayList<>();
+        }
+
+        adapter = new Adapter(list,this);
+        recyclerView.setAdapter(adapter);
+        layoutAnimation(recyclerView);
+        adapter.notifyDataSetChanged();
+
+        card_view_add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog add_task_dialog = new Dialog(MainActivity.this);
+                add_task_dialog.setContentView(R.layout.add_task_dialog);
+                add_task_dialog.setCanceledOnTouchOutside(false);
+                add_task_dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                WindowManager.LayoutParams layout_params = new WindowManager.LayoutParams();
+                layout_params.copyFrom(add_task_dialog.getWindow().getAttributes());
+                layout_params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                layout_params.height = WindowManager.LayoutParams.MATCH_PARENT;
+                add_task_dialog.getWindow().setAttributes(layout_params);
+                EditText et_add_here = add_task_dialog.findViewById(R.id.et_add_here);
+                TextView tv_add_to_list = add_task_dialog.findViewById(R.id.tv_add_to_list);
+                add_task_dialog.show();
+                tv_add_to_list.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (!et_add_here.getText().toString().isEmpty()){
+                            list.add(String.valueOf(et_add_here.getText()));
+                            adapter.notifyItemInserted(list.size()-1);
+                            adapter.notifyDataSetChanged();
+
+                            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.todoapp", Context.MODE_PRIVATE);
+                            HashSet<String> set = new HashSet(list);
+                            sharedPreferences.edit().putStringSet("addNote", set).apply();
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Can't create empty events!", Toast.LENGTH_SHORT).show();
+                        }
+                        add_task_dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
+
+    private void layoutAnimation(RecyclerView recyclerView){
+        Context context = recyclerView.getContext();
+        LayoutAnimationController layoutAnimationController = AnimationUtils.loadLayoutAnimation(context,R.anim.layout_fall_down);
+        recyclerView.setLayoutAnimation(layoutAnimationController);
+        recyclerView.getAdapter().notifyDataSetChanged();
+        recyclerView.scheduleLayoutAnimation();
+    }
+}
